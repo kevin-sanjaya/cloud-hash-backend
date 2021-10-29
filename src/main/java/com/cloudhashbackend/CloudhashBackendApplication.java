7a0b16a296148ce56c248292aeeb653f8714aef8
kevin.sanjaya <kevin.sanjaya@ing.com>
2021-10-29 20:08:46 +0200

    Initial commit

---
+++ src/main/java/com/cloudhashbackend/CloudhashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cloudhashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudhashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudhashBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudhashbackend/CloudhashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudhashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudhashBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
